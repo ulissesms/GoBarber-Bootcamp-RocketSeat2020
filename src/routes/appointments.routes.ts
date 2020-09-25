69aa1987c5e3da15c343be42ce9b5fd67cef1d85
ulissesms <ulissesms@MacBook-Pro-de-Ulisses.local>
2020-09-24 23:50:34 -0300

    created appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,12 +1,35 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
-  return response.json({ message: 'Hello world' });
+  const { provider, date } = request.body;
+  const parseDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parseDate, appointment.date),
+  );
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'this applintment is already booked.' });
+  }
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parseDate,
+  };
+  appointments.push(appointment);
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
