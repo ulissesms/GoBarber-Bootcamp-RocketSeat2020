899ef3774b560f53b1730ce198c1a03d847d6fb6
ulissesms <ulisses.sousa@edu.unipar.br>
2020-11-18 21:21:40 -0300

    ajutiment in appointments

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,3 @@
+import Appointment from '../models/Appointment';
+
+class AppointmentRepository {}
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,33 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 
 const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
-  const parseDate = startOfHour(parseISO(date));
+  const parsedDate = startOfHour(parseISO(date));
   const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parseDate, appointment.date),
+    isEqual(parsedDate, appointment.date),
   );
   if (findAppointmentInSameDate) {
     return response
       .status(400)
       .json({ message: 'this applintment is already booked.' });
   }
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parseDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
   appointments.push(appointment);
   return response.json(appointment);
 });
